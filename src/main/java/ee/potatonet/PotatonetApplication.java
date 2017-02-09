45ec42be9a497f4dd0cff339d003ae19fc0aef1a
Tiit Oja <tiit.oja@gmail.com>
2017-02-09 16:57:30 +0200

    Add initial Spring Boot project

---
+++ src/main/java/ee/potatonet/PotatonetApplication.java
@@ -0,0 +1,12 @@
+package ee.potatonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatonetApplication.class, args);
+	}
+}
