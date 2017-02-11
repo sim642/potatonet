052c6da29216263c7269db13c7b4fd9ef2af7a23
Simmo Saan <simmo.saan@gmail.com>
2017-02-12 00:49:30 +0200

    Create eid package containing EID details extractor

---
+++ src/main/java/ee/potatonet/eid/EIDDetailsX509PrincipalExtractor.java
@@ -0,0 +1,35 @@
+package ee.potatonet.eid;
+
+import java.security.cert.X509Certificate;
+import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor;
+import org.springframework.security.web.authentication.preauth.x509.X509PrincipalExtractor;
+
+public class EIDDetailsX509PrincipalExtractor implements X509PrincipalExtractor {
+  private final SubjectDnX509PrincipalExtractor idCodeExtractor;
+  private final SubjectDnX509PrincipalExtractor givenNameExtractor;
+  private final SubjectDnX509PrincipalExtractor surnameExtractor;
+  private final EIDEmailX509PrincipalExtractor emailExtractor;
+
+  public EIDDetailsX509PrincipalExtractor() {
+    idCodeExtractor = new SubjectDnX509PrincipalExtractor();
+    idCodeExtractor.setSubjectDnRegex("serialNumber=(\\d{11})");
+
+    givenNameExtractor = new SubjectDnX509PrincipalExtractor();
+    givenNameExtractor.setSubjectDnRegex("givenName=(.*?)(?:,|$)");
+
+    surnameExtractor = new SubjectDnX509PrincipalExtractor();
+    surnameExtractor.setSubjectDnRegex("surName=(.*?)(?:,|$)");
+
+    emailExtractor = new EIDEmailX509PrincipalExtractor();
+  }
+
+  @Override
+  public EIDDetails extractPrincipal(X509Certificate cert) {
+    String idCode = (String) idCodeExtractor.extractPrincipal(cert);
+    String givenName = (String) givenNameExtractor.extractPrincipal(cert);
+    String surname = (String) surnameExtractor.extractPrincipal(cert);
+    String email = emailExtractor.extractPrincipal(cert);
+
+    return new EIDDetails(idCode, givenName, surname, email);
+  }
+}
