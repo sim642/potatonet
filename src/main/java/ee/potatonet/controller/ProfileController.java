6c80ee2538b884406d3a90a5c32280357679ac12
veikokaap <veikokaap@gmail.com>
2017-02-16 16:57:33 +0200

    Make profile page show actual data

--- src/main/java/ee/potatonet/controller/ProfileController.java
+++ src/main/java/ee/potatonet/controller/ProfileController.java
@@ -8,34 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import ee.potatonet.data.User;
-import ee.potatonet.data.repos.PostRepository;
 import ee.potatonet.data.repos.UserRepository;
 
 @Controller
 public class ProfileController {
 
-  private final PostRepository postRepository;
   private final UserRepository userRepository;
 
   @Autowired
-  public ProfileController(PostRepository postRepository, UserRepository userRepository) {
-    this.postRepository = postRepository;
+  public ProfileController(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
   @RequestMapping(value = "/profile/{id}", method = RequestMethod.GET)
   public String doGet(@PathVariable("id") long id, Model model) {
-    User one = userRepository.findOne(id);
-    System.out.println(one);
-    model.addAttribute("posts", postRepository.findAllPostsByUserOrderByCreationDateTimeDesc(one));
+    model.addAttribute("user", userRepository.findOne(id));
 
     return "profile";
   }
 
   @RequestMapping(value = "/profile", method = RequestMethod.GET)
   public String doGet(@CurrentUser User currentUser, Model model) {
-    model.addAttribute("posts", postRepository.findAllPostsByUserOrderByCreationDateTimeDesc(currentUser));
-    // TODO: Add logic
+    model.addAttribute("user", currentUser);
+
     return "profile";
   }
 }
