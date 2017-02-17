a39ee3c307b4972ead3d1d134e915ed7f65f52f7
Tiit Oja <tiit.oja@gmail.com>
2017-02-17 20:36:08 +0200

    Set correct domain ports for production environment

--- src/main/java/ee/potatonet/PotatonetApplication.java
+++ src/main/java/ee/potatonet/PotatonetApplication.java
@@ -61,7 +61,7 @@ public class PotatonetApplication {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
 		connector.setPort(8080);
-		connector.setRedirectPort(8443);
+		connector.setRedirectPort(443);
 		return connector;
 	}
 
