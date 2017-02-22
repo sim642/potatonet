a2ff778bd4d88c8decd0da713b6a525405fcbe6e
Simmo Saan <simmo.saan@gmail.com>
2017-02-22 13:16:31 +0200

    Use model for password setting

--- src/main/java/ee/potatonet/controller/SettingsController.java
+++ src/main/java/ee/potatonet/controller/SettingsController.java
@@ -1,13 +1,14 @@
 package ee.potatonet.controller;
 
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import ee.potatonet.data.User;
 import ee.potatonet.data.repos.UserRepository;
@@ -23,19 +24,44 @@ public class SettingsController {
 
   @RequestMapping(value = "/settings", method = RequestMethod.GET)
   public String doGet(Model model) {
-    // TODO: Add logic
+    model.addAttribute("passwordSettings", new PasswordSettings());
     return "settings";
   }
 
   @PostMapping("/settings")
-  public String doPost(@CurrentUser User currentUser, @RequestParam("inputPassword") String password, @RequestParam("inputPassword2") String password2) {
-    if (!password.equals(password2)) {
+  public String doPost(@ModelAttribute PasswordSettings passwordSettings, @CurrentUser User currentUser) {
+    if (!passwordSettings.areMatching()) {
       return "redirect:/settings?error";
     }
 
-    currentUser.setPassword(passwordEncoder.encode(password));
+    currentUser.setPassword(passwordEncoder.encode(passwordSettings.getPassword()));
     userRepository.save(currentUser);
 
     return "redirect:/settings?success";
+  }
+
+  public static class PasswordSettings {
+    private String password;
+    private String passwordConfirm;
+
+    public boolean areMatching() {
+      return Objects.equals(password, passwordConfirm);
+    }
+
+    public String getPassword() {
+      return password;
+    }
+
+    public void setPassword(String password) {
+      this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+      return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+      this.passwordConfirm = passwordConfirm;
+    }
   }
 }
