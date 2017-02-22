5c8ff9e6a52fec2f355540474169ac602071c2fb
veikokaap <veikokaap@gmail.com>
2017-02-22 16:58:33 +0200

    Fix hibernate lazy loading for CurrentUserControllerAdvice

--- src/main/java/ee/potatonet/controller/CurrentUserControllerAdvice.java
+++ src/main/java/ee/potatonet/controller/CurrentUserControllerAdvice.java
@@ -1,15 +1,28 @@
 package ee.potatonet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import ee.potatonet.data.User;
+import ee.potatonet.data.repos.UserRepository;
 
 @ControllerAdvice
 public class CurrentUserControllerAdvice {
+  
+  private final UserRepository userRepository;
+
+  @Autowired
+  public CurrentUserControllerAdvice(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
   @ModelAttribute
   public void currentUserAttribute(@CurrentUser User currentUser, Model model) {
+    if (currentUser != null) {
+      currentUser = userRepository.findOne(currentUser.getId());
+    }
     model.addAttribute("currentUser", currentUser);
   }
 }
