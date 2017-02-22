a858b244d19a10b84739d7a5ad697ee02882f26f
veikokaap <veikokaap@gmail.com>
2017-02-22 17:23:00 +0200

    Implement Add Friend

--- src/main/java/ee/potatonet/controller/ProfileController.java
+++ src/main/java/ee/potatonet/controller/ProfileController.java
@@ -1,9 +1,14 @@
 package ee.potatonet.controller;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -32,5 +37,16 @@ public class ProfileController {
     model.addAttribute("user", userRepository.findOne(currentUser.getId()));
 
     return "profile";
+  }
+
+  @PostMapping(value = "/profile/add/{id}")
+  public String doGet(@CurrentUser User currentUser, @PathVariable("id") long id, Model model) {
+    currentUser = userRepository.findOne(currentUser.getId());
+    User user = userRepository.findOne(id);
+    user.getIncomingFriendRequests().add(currentUser);
+    userRepository.save(user);
+    userRepository.save(currentUser);
+
+    return "redirect:/profile/" + id;
   }
 }
