8f9f7b24e592a2ef21d9120831faaaf320a2ec22
veikokaap <veikokaap@gmail.com>
2017-02-22 17:27:42 +0200

    Fix error on posting

--- src/main/java/ee/potatonet/controller/FeedController.java
+++ src/main/java/ee/potatonet/controller/FeedController.java
@@ -12,19 +12,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import ee.potatonet.data.Post;
 import ee.potatonet.data.User;
 import ee.potatonet.data.repos.PostRepository;
+import ee.potatonet.data.repos.UserRepository;
 
 @Controller
 public class FeedController {
 
   private final PostRepository postRepository;
+  private final UserRepository userRepository;
 
   @Autowired
-  public FeedController(PostRepository postRepository) {
+  public FeedController(PostRepository postRepository, UserRepository userRepository) {
     this.postRepository = postRepository;
+    this.userRepository = userRepository;
   }
 
   @RequestMapping(value = "/feed", method = RequestMethod.GET)
   public String doGet(@CurrentUser User currentUser, Model model) {
+    currentUser = userRepository.findOne(currentUser.getId());
     model.addAttribute("post", new Post());
     model.addAttribute("posts", postRepository.findAllPostsByFriendsAndMe(currentUser));
     return "feed";
@@ -32,6 +36,7 @@ public class FeedController {
   
   @RequestMapping(value = "/feed", method = RequestMethod.POST)
   public String savePost(@CurrentUser User currentUser, @ModelAttribute Post post) {
+    currentUser = userRepository.findOne(currentUser.getId());
     post.setUser(currentUser);
     post.setCreationDateTime(ZonedDateTime.now());
     Post saved = postRepository.save(post);
