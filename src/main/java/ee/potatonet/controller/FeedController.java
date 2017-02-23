25c3768ca35fc7068079f8093f800ea8d8d931bc
Simmo Saan <simmo.saan@gmail.com>
2017-02-23 11:32:28 +0200

    Try rendering a parametrized thymeleaf template fragment to String

--- src/main/java/ee/potatonet/controller/FeedController.java
+++ src/main/java/ee/potatonet/controller/FeedController.java
@@ -1,18 +1,22 @@
 package ee.potatonet.controller;
 
 
+import java.security.Principal;
 import java.time.ZonedDateTime;
-
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.thymeleaf.context.ExpressionContext;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.expression.ThymeleafEvaluationContext;
 
 import ee.potatonet.data.Post;
 import ee.potatonet.data.User;
@@ -24,11 +28,13 @@ public class FeedController {
   private final PostRepository postRepository;
 
   private final SimpMessagingTemplate simpMessagingTemplate;
+  private final SpringTemplateEngine springTemplateEngine;
 
   @Autowired
-  public FeedController(PostRepository postRepository, SimpMessagingTemplate simpMessagingTemplate) {
+  public FeedController(PostRepository postRepository, SimpMessagingTemplate simpMessagingTemplate, SpringTemplateEngine springTemplateEngine) {
     this.postRepository = postRepository;
     this.simpMessagingTemplate = simpMessagingTemplate;
+    this.springTemplateEngine = springTemplateEngine;
   }
 
   @RequestMapping(value = "/feed", method = RequestMethod.GET)
@@ -50,9 +56,22 @@ public class FeedController {
     return "redirect:/feed";
   }
 
+  @Autowired
+  private ApplicationContext applicationContext;
+
   @MessageMapping("/fromSockJSClient")
-  public void handlePost() {
+  public void handlePost(@CurrentUser User currentUser, Principal principal) {
     simpMessagingTemplate.convertAndSend("/feed/websocket", "{\"content\": \"stuff\"}");
+
+    User user = (User) ((Authentication) principal).getPrincipal();
+
+    ExpressionContext context = new ExpressionContext(springTemplateEngine.getConfiguration());
+    context.setVariable("postInfo", new Post(user, "test"));
+    final ThymeleafEvaluationContext evaluationContext =
+        new ThymeleafEvaluationContext(applicationContext, null);
+    context.setVariable(ThymeleafEvaluationContext.THYMELEAF_EVALUATION_CONTEXT_CONTEXT_VARIABLE_NAME, evaluationContext);
+    String output = springTemplateEngine.process("common", Collections.singleton("post"), context);
+    System.out.println(output);
   }
   
 }
