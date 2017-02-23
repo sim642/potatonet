73726e093782ac143aa8ad01f41a0c11a039cd0a
Simmo Saan <simmo.saan@gmail.com>
2017-02-23 12:49:49 +0200

    Extract template rendering to string to service

--- src/main/java/ee/potatonet/controller/FeedController.java
+++ src/main/java/ee/potatonet/controller/FeedController.java
@@ -4,7 +4,6 @@ package ee.potatonet.controller;
 import java.time.ZonedDateTime;
 import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -13,9 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.ExpressionContext;
-import org.thymeleaf.spring4.expression.ThymeleafEvaluationContext;
 
+import ee.potatonet.TemplateRenderService;
 import ee.potatonet.data.Post;
 import ee.potatonet.data.User;
 import ee.potatonet.data.repos.PostRepository;
@@ -26,13 +24,14 @@ public class FeedController {
   private final PostRepository postRepository;
 
   private final SimpMessagingTemplate simpMessagingTemplate;
-  private final TemplateEngine templateEngine;
+
+  private final TemplateRenderService templateRenderService;
 
   @Autowired
-  public FeedController(PostRepository postRepository, SimpMessagingTemplate simpMessagingTemplate, TemplateEngine templateEngine) {
+  public FeedController(PostRepository postRepository, SimpMessagingTemplate simpMessagingTemplate, TemplateEngine templateEngine, TemplateRenderService templateRenderService) {
     this.postRepository = postRepository;
     this.simpMessagingTemplate = simpMessagingTemplate;
-    this.templateEngine = templateEngine;
+    this.templateRenderService = templateRenderService;
   }
 
   @RequestMapping(value = "/feed", method = RequestMethod.GET)
@@ -54,19 +53,12 @@ public class FeedController {
     return "redirect:/feed";
   }
 
-  @Autowired
-  private ApplicationContext applicationContext;
-
   @MessageMapping("/fromSockJSClient")
   public void handlePost(@CurrentUser User currentUser) {
     simpMessagingTemplate.convertAndSend("/feed/websocket", "{\"content\": \"stuff\"}");
 
-    ExpressionContext context = new ExpressionContext(templateEngine.getConfiguration());
-    context.setVariable("postInfo", new Post(currentUser, "test"));
-    final ThymeleafEvaluationContext evaluationContext =
-        new ThymeleafEvaluationContext(applicationContext, null);
-    context.setVariable(ThymeleafEvaluationContext.THYMELEAF_EVALUATION_CONTEXT_CONTEXT_VARIABLE_NAME, evaluationContext);
-    String output = templateEngine.process("common", Collections.singleton("post"), context);
+    Post post = new Post(currentUser, "test");
+    String output = templateRenderService.render("common", Collections.singleton("post"), Collections.singletonMap("postInfo", post));
     System.out.println(output);
   }
   
