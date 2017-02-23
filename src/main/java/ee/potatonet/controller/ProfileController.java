33e3bf9a6c9f19f2f35662983ea3ea5e8c5a4abb
Tiit Oja <tiit.oja@gmail.com>
2017-02-23 20:15:49 +0200

    Fix post ordering on profile view (#33)

--- src/main/java/ee/potatonet/controller/ProfileController.java
+++ src/main/java/ee/potatonet/controller/ProfileController.java
@@ -1,40 +1,41 @@
 package ee.potatonet.controller;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import ee.potatonet.data.User;
+import ee.potatonet.data.repos.PostRepository;
+import ee.potatonet.data.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import ee.potatonet.data.User;
-import ee.potatonet.data.repos.UserRepository;
 
 @Controller
 public class ProfileController {
 
   private final UserRepository userRepository;
+  private final PostRepository postRepository;
 
   @Autowired
-  public ProfileController(UserRepository userRepository) {
+  public ProfileController(UserRepository userRepository, PostRepository postRepository) {
     this.userRepository = userRepository;
+    this.postRepository = postRepository;
   }
 
   @RequestMapping(value = "/profile/{id}", method = RequestMethod.GET)
   public String doGet(@PathVariable("id") long id, Model model) {
-    model.addAttribute("user", userRepository.findOne(id));
+    User user = userRepository.findOne(id);
+    model.addAttribute("user", user);
+    model.addAttribute("posts", postRepository.findAllPostsByUserOrderByCreationDateTimeDesc(user));
 
     return "profile";
   }
 
   @RequestMapping(value = "/profile", method = RequestMethod.GET)
   public String doGet(@CurrentUser User currentUser, Model model) {
-    model.addAttribute("user", userRepository.findOne(currentUser.getId()));
+    model.addAttribute("user", currentUser);
+    model.addAttribute("posts", postRepository.findAllPostsByUserOrderByCreationDateTimeDesc(currentUser));
 
     return "profile";
   }
