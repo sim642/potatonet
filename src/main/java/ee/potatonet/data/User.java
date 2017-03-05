64ffbd05975f59a29f7b7b64b1d652b0c207186b
Veiko Kääp <veikokaap@gmail.com>
2017-03-05 17:13:18 +0200

    Fix User never returning authorities

--- src/main/java/ee/potatonet/data/User.java
+++ src/main/java/ee/potatonet/data/User.java
@@ -206,7 +206,7 @@ public class User implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    return authorities;
   }
 
   @Override
