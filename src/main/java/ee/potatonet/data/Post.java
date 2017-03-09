e025feb377a1aa7f8b2e0e141b110ed484b63313
Simmo Saan <simmo.saan@gmail.com>
2017-03-09 17:33:21 +0200

    Format post dates nicely

--- src/main/java/ee/potatonet/data/Post.java
+++ src/main/java/ee/potatonet/data/Post.java
@@ -1,9 +1,17 @@
 package ee.potatonet.data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.ZonedDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Post {
@@ -19,7 +27,8 @@ public class Post {
   @NotNull
   @Size(min = 1, max = 32700)
   private String content;
-  
+
+  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private ZonedDateTime creationDateTime;
 
   public Post(User user, String content) {
