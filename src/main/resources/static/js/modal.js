5ff4361c8bb9b7d1dbcc0d88565d46d2067bd2b2
Simmo Saan <simmo.saan@gmail.com>
2017-03-10 22:56:23 +0200

    Add history state handling to map modal opening (#19)

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,13 +1,40 @@
+var mapModalPopping = false; // hack to allow modal's event handlers to know whether modal show/hide was triggered by history state pop
+
 $('#mapModal').on('show.bs.modal', function (event) {
   var post = $(event.relatedTarget);
   var latitude = post.data('latitude');
   var longitude = post.data('longitude');
   initMap(longitude, latitude);
+
+  if (!mapModalPopping) {
+    var $post = post.closest(".panel-post");
+    var postId = $post.attr("data-post-id");
+    window.history.pushState({map: postId}, {}, "/posts/" + postId);
+  }
+  mapModalPopping = false;
 });
 
 $('#mapModal').on('shown.bs.modal', function () {
   google.maps.event.trigger(map, "resize");
 });
+
+$('#mapModal').on('hide.bs.modal', function () {
+  if (!mapModalPopping) {
+    window.history.back();
+  }
+  mapModalPopping = false;
+});
+
+window.onpopstate = function (event) {
+  mapModalPopping = true;
+  if (event.state !== null && ((postId = event.state.map) !== null)) {
+    var $post = $(".panel-post[data-post-id='" + postId + "']");
+    $('#mapModal').modal('show', $post.find(".btn-modal-map"));
+  }
+  else {
+    $('#mapModal').modal('hide');
+  }
+};
 
 function initMap(longitude, latitude) {
   var postLocation = {lat: latitude, lng: longitude};
