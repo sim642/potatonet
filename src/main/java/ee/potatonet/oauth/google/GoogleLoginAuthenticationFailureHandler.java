ea877ea1f86c5efe95e73dfac560ff008a77b4c4
Veiko Kääp <veikokaap@gmail.com>
2017-03-10 21:28:32 +0200

    Fix formatting (#47)

--- src/main/java/ee/potatonet/oauth/google/GoogleLoginAuthenticationFailureHandler.java
+++ src/main/java/ee/potatonet/oauth/google/GoogleLoginAuthenticationFailureHandler.java
@@ -9,8 +9,7 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationFa
 
 public class GoogleLoginAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 	@Override
-	public void onAuthenticationFailure(HttpServletRequest request,
-																			HttpServletResponse response, AuthenticationException exception)
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
 			throws IOException, ServletException {
 		getRedirectStrategy().sendRedirect(
 				request, response,
