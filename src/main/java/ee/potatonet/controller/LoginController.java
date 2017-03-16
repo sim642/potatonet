2d83a8d8eb0c045b1a5d17f1ff99982354a5dd9c
Simmo Saan <simmo.saan@gmail.com>
2017-03-16 15:06:02 +0200

    Merge GoogleLoginController into LoginController (#54)

--- src/main/java/ee/potatonet/controller/LoginController.java
+++ src/main/java/ee/potatonet/controller/LoginController.java
@@ -1,17 +1,23 @@
 package ee.potatonet.controller;
 
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
 
+import ee.potatonet.auth.google.DisabledAuthenticationToken;
 import ee.potatonet.controller.advice.CurrentUser;
 import ee.potatonet.data.model.User;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 
-  @RequestMapping(value = "/login")
+  @GetMapping
   public String doGet(@CurrentUser User currentUser, Model model) {
     if (currentUser == null)
       return "login";
@@ -19,9 +25,22 @@ public class LoginController {
       return "redirect:/";
   }
 
-  @RequestMapping("/login/eid")
+  @GetMapping("/eid")
   @ResponseBody
   public void doGetEid() {
 
+  }
+
+  @GetMapping("/google/pre")
+  public String doGetGooglePre() {
+    DisabledAuthenticationToken preAuthentication = new DisabledAuthenticationToken(
+        RequestContextHolder.currentRequestAttributes().getSessionId(),
+        "N/A",
+        AuthorityUtils.createAuthorityList("ROLE_PRE_GOOGLE")
+    );
+
+    SecurityContextHolder.getContext().setAuthentication(preAuthentication);
+
+    return "redirect:/login/google";
   }
 }
