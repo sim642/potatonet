0f0a0f064cdbd98c5ac4076060a86414f21a8e35
Simmo Saan <simmo.saan@gmail.com>
2017-03-16 14:47:16 +0200

    Move configuration beans out of PotatonetApplication (#54)

--- src/main/java/ee/potatonet/configuration/WebMvcConfiguration.java
+++ src/main/java/ee/potatonet/configuration/WebMvcConfiguration.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+
+import ee.potatonet.data.model.Language;
 
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
@@ -28,5 +32,12 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
   @Override
   public Validator getValidator() {
     return validator();
+  }
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+    cookieLocaleResolver.setDefaultLocale(Language.EN.getLocale());
+    return cookieLocaleResolver;
   }
 }
