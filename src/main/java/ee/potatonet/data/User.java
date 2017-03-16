9123fd929c9d28f1d9471a97d3a999935a676e89
Simmo Saan <simmo.saan@gmail.com>
2017-03-16 14:14:03 +0200

    Extract authorities from User model (#54)

--- src/main/java/ee/potatonet/data/User.java
+++ src/main/java/ee/potatonet/data/User.java
@@ -13,26 +13,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-import javax.persistence.Transient;
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.SpringSecurityCoreVersion;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.util.Assert;
 
 import ee.potatonet.auth.eid.EIDDetails;
 
 @Entity
-public class User implements UserDetails {
+public class User extends TransientAuthoritiesUser {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -63,9 +52,6 @@ public class User implements UserDetails {
 
   @ManyToMany(mappedBy = "incomingFriendRequests")
   private Set<User> outgoingFriendRequests;
-  
-  @Transient
-  private Set<? extends GrantedAuthority> authorities;
 
   public User() {
 
@@ -137,28 +123,6 @@ public class User implements UserDetails {
     this.friends = friends;
   }
 
-  //Copy-paste from spring security User
-  private static SortedSet<GrantedAuthority> sortAuthorities(
-      Collection<? extends GrantedAuthority> authorities) {
-    Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
-    // Ensure array iteration order is predictable (as per
-    // UserDetails.getAuthorities() contract and SEC-717)
-    SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<GrantedAuthority>(
-        new AuthorityComparator());
-
-    for (GrantedAuthority grantedAuthority : authorities) {
-      Assert.notNull(grantedAuthority,
-          "GrantedAuthority list cannot contain any null elements");
-      sortedAuthorities.add(grantedAuthority);
-    }
-
-    return sortedAuthorities;
-  }
-
-  public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
-    this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
-  }
-
   public Language getLanguage() {
     return language;
   }
@@ -173,27 +137,6 @@ public class User implements UserDetails {
 
   public void setGoogleId(String googleId) {
     this.googleId = googleId;
-  }
-
-  private static class AuthorityComparator implements Comparator<GrantedAuthority>,
-      Serializable {
-    private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
-
-    public int compare(GrantedAuthority g1, GrantedAuthority g2) {
-      // Neither should ever be null as each entry is checked before adding it to
-      // the set.
-      // If the authority is null, it is a custom authority and should precede
-      // others.
-      if (g2.getAuthority() == null) {
-        return -1;
-      }
-
-      if (g1.getAuthority() == null) {
-        return 1;
-      }
-
-      return g1.getAuthority().compareTo(g2.getAuthority());
-    }
   }
 
   @Override
@@ -221,11 +164,6 @@ public class User implements UserDetails {
         "id=" + id +
         ", eid=" + eid +
         '}';
-  }
-
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return authorities;
   }
 
   @Override
