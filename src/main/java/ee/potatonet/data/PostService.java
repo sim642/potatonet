3c4931a42017d2eb0d83b20cd8aa9e977257c10f
Simmo Saan <simmo.saan@gmail.com>
2017-03-19 20:12:42 +0200

    Replace post panel pushing over websocket with AJAX request (closes #58)

--- src/main/java/ee/potatonet/data/PostService.java
+++ src/main/java/ee/potatonet/data/PostService.java
@@ -1,16 +1,12 @@
 package ee.potatonet.data;
 
 import java.time.ZonedDateTime;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ee.potatonet.TemplateRenderService;
 import ee.potatonet.data.repos.PostRepository;
 
 @Service
@@ -21,14 +17,12 @@ public class PostService {
 
   private final PostRepository postRepository;
   private final UserService userService;
-  private final TemplateRenderService templateRenderService;
   private final SimpMessagingTemplate simpMessagingTemplate;
 
   @Autowired
-  public PostService(PostRepository postRepository, UserService userService, TemplateRenderService templateRenderService, SimpMessagingTemplate simpMessagingTemplate) {
+  public PostService(PostRepository postRepository, UserService userService, SimpMessagingTemplate simpMessagingTemplate) {
     this.postRepository = postRepository;
     this.userService = userService;
-    this.templateRenderService = templateRenderService;
     this.simpMessagingTemplate = simpMessagingTemplate;
   }
 
@@ -68,8 +62,7 @@ public class PostService {
     user.getPosts().add(post);
     userService.save(user);
 
-    String output = templateRenderService.render("common", Collections.singleton("postPanel"), Collections.singletonMap("post", post));
-    simpMessagingTemplate.convertAndSend("/topic/posts/" + user.getId(), output);
+    simpMessagingTemplate.convertAndSend("/topic/posts/" + user.getId(), post.getId());
   }
 
   public List<Coordinates> getAllPostCoordinates() {
