9eb23b19f23cba19c02e5da4d2fbfff266a2ef9b
Simmo Saan <simmo.saan@gmail.com>
2017-03-21 17:12:21 +0200

    Initialize BouncyCastle statically to ensure BC security provider in testing

--- src/main/java/ee/potatonet/PotatonetApplication.java
+++ src/main/java/ee/potatonet/PotatonetApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PotatonetApplication {
 
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
+
 	public static void main(String[] args) {
 		System.setProperty("timestamp", String.valueOf(System.currentTimeMillis()));
-
-		Security.addProvider(new BouncyCastleProvider());
 
 		SpringApplication.run(PotatonetApplication.class, args);
 	}
