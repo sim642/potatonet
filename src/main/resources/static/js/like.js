b113ffbbeb310c4559f9b8b6cfa649258094353c
Simmo Saan <simmo.saan@gmail.com>
2017-03-26 15:48:58 +0300

    Remove duplicate IDs on like buttons (WCAG)

--- src/main/resources/static/js/like.js
+++ src/main/resources/static/js/like.js
@@ -3,7 +3,7 @@ $(function () {
   $(document).on("submit", ".like", function (event) {
     var panelpost = $(event.target).closest(".panel-post");
     var post_id = panelpost.attr("data-post-id");
-    var button = panelpost.find("#likeButton");
+    var button = panelpost.find(".like-btn");
 
     $.ajax({
       url: '/like/' + post_id,
