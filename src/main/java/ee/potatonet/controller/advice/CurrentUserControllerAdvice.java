f0619dd20b4ac2efb12ce86fe26163f02020fc9d
Simmo Saan <simmo.saan@gmail.com>
2017-04-05 17:36:21 +0300

    Use comment fragment for comment form (#43)

--- src/main/java/ee/potatonet/controller/advice/CurrentUserControllerAdvice.java
+++ src/main/java/ee/potatonet/controller/advice/CurrentUserControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import ee.potatonet.data.model.Comment;
 import ee.potatonet.data.model.User;
 import ee.potatonet.data.service.UserService;
 
@@ -12,6 +13,7 @@ import ee.potatonet.data.service.UserService;
 public class CurrentUserControllerAdvice {
   
   private final UserService userService;
+
 
   @Autowired
   public CurrentUserControllerAdvice(UserService userService) {
@@ -24,6 +26,7 @@ public class CurrentUserControllerAdvice {
       currentUser = userService.find(currentUser);
       model.addAttribute("currentUser", currentUser);
       model.addAttribute("incomingFriendRequestCount", userService.countUserIncomingFriendRequests(currentUser));
+      model.addAttribute("newComment", new Comment(null, currentUser, ""));
     }
   }
 }
