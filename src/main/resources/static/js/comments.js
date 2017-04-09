f67473e8b59f7104788c4f3782e0dc84f6491804
Simmo Saan <simmo.saan@gmail.com>
2017-04-09 18:45:47 +0300

    Allow commenting with enter key (closes #68)

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -3,9 +3,20 @@ $(function () {
   var stomp = Stomp.over(sockJs);
 
   window.subscribeComments = function subscribeComments($post) {
-    var $btnComment = $post.find(".btn-comment");
+    var $formComment = $post.find(".form-comment");
+    var $btnComment = $formComment.find(".btn-comment");
+    var $textarea = $formComment.find("textarea");
+
+    $textarea.keypress(function (event) {
+      if (event.keyCode == 13 && !event.shiftKey) {
+        $formComment.submit();
+        event.preventDefault();
+        return false;
+      }
+    });
+
     $btnComment.attr("disabled", true);
-    $post.find("textarea").keyup(function (event) {
+    $textarea.keyup(function (event) {
       $btnComment.attr("disabled", $(this).val().trim().length == 0);
     });
 
@@ -29,27 +40,29 @@ $(function () {
     var $post = $form.closest(".panel-post");
     var postId = $post.attr("data-post-id");
     var $content = $form.find("textarea");
-    $.post("/posts/" + postId + "/comments", $form.serialize())
-        .fail(function (jqXHR) {
-          // http://stackoverflow.com/a/28404728
-          switch (jqXHR.readyState) {
-            case 0:
-              alert("Network error");
-              break;
+    if ($content.val().trim().length != 0) {
+      $.post("/posts/" + postId + "/comments", $form.serialize())
+          .fail(function (jqXHR) {
+            // http://stackoverflow.com/a/28404728
+            switch (jqXHR.readyState) {
+              case 0:
+                alert("Network error");
+                break;
 
-            case 4:
-              alert("HTTP error (" + jqXHR.status + ")");
-              break;
+              case 4:
+                alert("HTTP error (" + jqXHR.status + ")");
+                break;
 
-            default:
-              alert("Unknown error");
-              break;
-          }
-        })
-        .done(function () {
-          $content.val("");
-          $form.find(".btn-comment").attr("disabled", true);
-        });
+              default:
+                alert("Unknown error");
+                break;
+            }
+          })
+          .done(function () {
+            $content.val("");
+            $form.find(".btn-comment").attr("disabled", true);
+          });
+    }
 
     return false;
   });
