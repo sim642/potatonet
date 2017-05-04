3681d1ffc5d32e89aec006fadeb0c313a9050f51
Simmo Saan <simmo.saan@gmail.com>
2017-05-04 11:42:52 +0300

    Move external JS libraries into lib subdirectory

--- src/main/resources/static/js/fallback-master.js
+++ src/main/resources/static/js/fallback-master.js
@@ -1,19 +1,19 @@
 fallback.load({
   jQuery: [
     'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
-    '/js/jquery-1.12.4.min.js'
+    '/js/lib/jquery-1.12.4.min.js'
   ],
   'jQuery.fn.modal': [
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
-    '/js/bootstrap.min.js'
+    '/js/lib/bootstrap.min.js'
   ],
   Stomp: [
     'https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.min.js',
-    '/js/stomp.min.js'
+    '/js/lib/stomp.min.js'
   ],
   SockJS: [
     'https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.1.2/sockjs.min.js',
-    '/js/sockjs.min.js'
+    '/js/lib/sockjs.min.js'
   ],
   masterjs: [
     '/js/master.js'
